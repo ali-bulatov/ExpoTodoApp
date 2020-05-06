ab677c52264caa4dfb0f40f296b473fd78146407
ali-bulatov <ali-bulatov@protonmail.com>
2020-05-06 07:50:50 -0400

    Main modal created using states

---
+++ tempData.js
@@ -0,0 +1,65 @@
+// Temporary data before Firebase connection]
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Make an App",
+                completed: true
+            },
+            {
+                title: "Go to Sleep",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Get Baloons",
+                completed: false
+            },
+            {
+                title: "Send Invintations",
+                completed: true
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: true
+            }
+        ]
+    }
+]
